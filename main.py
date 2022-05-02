a510fd7e01641a8ab9fa34a84383b49a26269442
dimitrisdiam <64299794+dimitrisdiam@users.noreply.github.com>
2022-05-02 17:54:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,22 +4,17 @@ import smtplib
 
 # Features about Authorization.
 headers = {
-            "Accept-Language": "el-GR,el;q=0.9",
+            "Accept-Language": "en,el;q=0.9",
             "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) "
-                          "Chrome/97.0.4692.71 Safari/537.36",
+                          "Chrome/101.0.4951.41 Safari/537.36",
 }
 
-
-response = requests.get("https://www.amazon.com/ILIFE-Pro-ElectroWall-Self-Charging-Tangle-Free/dp/B085VR2WYR/ref=sr_1_"
-                        "1_sspa?crid=3FHLBBH3997JR&keywords=Robotic%2BVacuum%2BCleaner&nav_sdd=aps&qid=1642067646&"
-                        "sprefix=vac&sr=8-1-spons&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUFOQVY1MjQ2OTFaVlEmZW5jcnlwdGVkSWQ9QT"
-                        "A0Mjc4MThLV0Q1Uk9aOEo3QVkmZW5jcnlwdGVkQWRJZD1BMDgwMDc5ODM5OTJRWURGODRJN0kmd2lkZ2V0TmFtZT1zcF9"
-                        "hdGYmYWN0aW9uPWNsaWNrUmVkaXJlY3QmZG9Ob3RMb2dDbGljaz10cnVl&th=1",
+response = requests.get("https://www.amazon.com/ILIFE-Pro-ElectroWall-Self-Charging-Tangle-Free/dp/B085VR2WYR?ref_=ast_sto_dp&th=1&psc=1_",
                         headers=headers)
 
 html_amazonpage = response.text
 soup = BeautifulSoup(html_amazonpage, "lxml")
-vacuum_price = float(soup.find(name="span", class_="a-price-whole").text.split(".")[0])
+vacuum_price = float(soup.find(name="span", class_="a-offscreen").text.replace("$",""))
 
 # Infos about email.
 SERVER = "smtp.gmail.com"
@@ -28,11 +23,10 @@ TO = "SECOND_EMAIL"
 SUBJECT = "Track the price of vacuum"
 
 msg = 'To:' + TO + '\n' + 'From: ' + FROM + '\n' + f"Subject:{SUBJECT}" + '\n'
-
 TEXT = msg + f"Hurry up!\n The price decreases. Now is {vacuum_price}!"
 
 # Procedure about sending.
-if vacuum_price < 100:
+if vacuum_price < 200:
     server = smtplib.SMTP(SERVER, 587)
     server.ehlo()
     server.starttls()
@@ -40,3 +34,4 @@ if vacuum_price < 100:
     server.login(FROM, "FIRST_EMAIL_PASSWORD")
     server.sendmail(FROM, TO, TEXT)
     server.quit()
+
